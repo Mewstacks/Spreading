5ba8d2c0349fff36d40ebd64e9b809a38535159c
Germano <germano2227@gmail.com>
2026-05-11 20:33:45 -0300

    nu como o gurizao ta avançado

--- python/scrapper_mercadolivre/scrapper.py
+++ python/scrapper_mercadolivre/scrapper.py
@@ -1,74 +1,44 @@
-from playwright.sync_api import sync_playwright
+import sys
 import os
+caminho_atual = os.path.dirname(os.path.abspath(__file__))
+caminho_raiz = os.path.dirname(caminho_atual)
+sys.path.append(caminho_raiz)
+from auxiliar import iniciar_browser, BrowserError
+import json
 
 
 class LoginError(Exception):
     """Exceção personalizada para erros de login."""
     pass
 
+class AuthError(Exception):
+    """Exceção personalizada para erros de autenticação, quando o ML bloqueia essa merda."""
+    pass
 
 
-def run_scrapper(tipo_produto):
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, args=["--disable-blink-features=AutomationControlled"])
-        
-        # Só tenta carregar se o arquivo realmente existir
-        if os.path.exists("auth.json"):
-            context = browser.new_context(storage_state="auth.json")
-        else:
-            context = browser.new_context()
 
-        page = context.new_page()
-        page.goto("https://www.mercadolivre.com.br/afiliados/hub")
-        login_field = page.get_by_test_id("user_id")
-        
+
+
+def afiliate_link_builder(link_base):    
+    with iniciar_browser(
+        auth_path=os.path.join(caminho_atual, "auth.json"),
+        headless=True, 
+        permissions=['clipboard-read', 'clipboard-write'], 
+    ) as (page, context):
         try:
-            if login_field.is_visible(timeout=5000):
-                print("Sessão não encontrada ou expirada. Faça o login manualmente...")
-                page.pause()
-                
-                context.storage_state(path="auth.json")
-            else:
-                print("Já estou logado! Prosseguindo...")
-        except Exception as e:
-            print("Já estou logado ou a página demorou a responder.")
-
-        page.get_by_role("textbox", name="Procurar").click()
-        page.get_by_role("textbox", name="Procurar").fill(tipo_produto)
-        page.get_by_role("textbox", name="Procurar").press("Enter")    
-        page.pause()
-        browser.close()
-
-
-
-
-def afiliate_link_builder(link_base):
-    link_final = ""
-    nav = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-    with sync_playwright() as p:
-        browser = p.chromium.launch(args=["--disable-blink-features=AutomationControlled"]) #headless=False,
-        if os.path.exists("auth.json"):
-            context = browser.new_context(storage_state="auth.json", permissions=['clipboard-read', 'clipboard-write'], user_agent=nav)
-        else:
-            context = browser.new_context(permissions=['clipboard-read', 'clipboard-write'], user_agent=nav)
-
-        page = context.new_page()
-        page.goto("https://www.mercadolivre.com.br/afiliados/linkbuilder#hub")
+            page.goto("https://www.mercadolivre.com.br/afiliados/linkbuilder#hub")
+        except:
+            raise AuthError("Não foi possível acessar o Link Builder. Verifique sua conexão e se a sessão está ativa, ou tente com o headless=False seu macaco.")
         
         login_field = page.get_by_test_id("user_id")
         if login_field.is_visible(timeout=10000):
-            browser.close()
             raise LoginError("Faça login e rode a função novamente para gerar o link de afiliado, seu bosta.")
-        else:
-            print("ta logado nessa porra")
+        print("ta logado nessa porra")
+        
         try:
             page.get_by_role("textbox", name="Insira 1 ou mais URLs").fill(link_base)
             page.get_by_role("button", name="Gerar").click()
 
-            '''link_completo = page.get_by_role("radio", name="Link completo")
-            link_completo.wait_for(state="visible", timeout=10000)
-            link_completo.check()'''
-        
             page.get_by_role("button", name="Copiar").click()
             link_final = page.evaluate("navigator.clipboard.readText()")
             
@@ -76,14 +46,16 @@ def afiliate_link_builder(link_base):
                 raise ValueError("Link de afiliado não gerado corretamente.")
                 
             print(f"Sucesso! O link gerado foi: {link_final}")
+            return link_final
 
         except Exception as e:
             print(f"Erro ao gerar o link de afiliado: {e}")
+            return None
         
-        finally:
-            browser.close()
-            
-        return link_final
 
 # Testando a função
 meu_link = afiliate_link_builder("https://www.mercadolivre.com.br/rob-aspirador-de-po-inteligente-wi-fi-kuanttum-mop-lava-e-seca-agua-quente-varre-aspira-3-em-1-robot-10000pa-autolimpante-x20-passa-pano-mapeamento-cor-preto-220v-com-base-estaco/p/MLB67993128?pdp_filters=item_id:MLB4609148189#is_advertising=true&searchVariation=MLB67993128&backend_model=search-backend&be_origin=backend&position=4&search_layout=grid&type=pad&tracking_id=4d631370-9e18-4e09-89bb-690d4b5c3fe8&ad_domain=VQCATCORE_LST&ad_position=4&ad_click_id=NTJjMTdiYmUtYzJiZC00MGUyLWE3MWUtZTUyNWNkNGI4NWQz")
+
+
+def cupons_extractor():
+    pass
